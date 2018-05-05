dc1d9efac6ca0c13e71217478612a0dfbdd97645
pytseng <tw.pytseng@gmail.com>
2018-05-05 15:19:20 -0400

    update logic

--- content.js
+++ content.js
@@ -15,46 +15,76 @@ document.addEventListener('mouseup', function (e) {
 
     renderBubble(X_pos, Y_pos, selection_str);
   }
-}, false);
+}, false);	
 
 // Close the bubble when we click on the screen.
 document.addEventListener('mousedown', function (e) {
   if (bubble_status==0){
   	bubbleDOM.style.visibility = 'hidden';	
   }
+
 }, false);
 
-bubbleDOM.addEventListener('mousedown', function(e) {
-	showImage();
-}, false);
+// bubbleDOM.addEventListener('mousedown', function(e) {
+// 	showImage();
+// }, false);
 
 
 
 // Move that bubble to the appropriate location.
 function renderBubble(mouseX, mouseY, selection_str) {
-  bubbleDOM.innerHTML = "<input type=\"image\" src=\"https://lh3.googleusercontent.com/20g2-tl9ftUe52-WrBNnFavTwbBzgQv8RHl4OpyZZGCigP8iv9-drCSHEjIvVWvQ1JQ4bkf3zktugIew7W8J=w1147-h605\"/>";
-  
+
   // bubbleDOM.innerHTML = selection_str;
   bubbleDOM.style.top = mouseY - 40 + 'px';
   bubbleDOM.style.left = mouseX + 'px';
   bubbleDOM.style.visibility = 'visible';
-  const result = politicians.find( politician => politician.name == selection_str);
-  if (typeof result !== 'undefined') { 
-
-  	bubbleDOM.innerHTML = "<img src=\"" + politician.img_url +" width=\"300px\" height=\"200px\" >";
-  }
-  // if (selection_str == "rand paul"){
-  // 	img_path = "images/Rand Paul.png";
-  // 	bubbleDOM.innerHTML = "<img src=\"https://pbs.twimg.com/profile_images/681152691461042177/_PrgDgFA_400x400.jpg\" width=\"200px\" height=\"200px\" >";
-  // }
+  bubbleDOM.innerHTML = "<img src=\"https://github.com/roycboyc/OpenPolitics/blob/master/logo_16.png?raw=true\">";
+  const result = politicians.find( politician => {
+  	if(politician.name.toLowerCase() == selection_str) {
+  		debugger;
+  		bubbleDOM.innerHTML = "<img src=\"" + politician.img_url +"\" width=\"300px\" height=\"200px\" >";	
+  	}
+  })
 }
 
-function showImage(){ 
-	console.log("cliiiick");
-	bubble_status = 1;
-	bubbleDOM.innerHTML = "<img src=\"https://pbs.twimg.com/profile_images/681152691461042177/_PrgDgFA_400x400.jpg\" width=\"200px\" height=\"200px\" >";
-	bubbleDOM.style.visibility = 'visible';
-}
+// function showImage(){ 
+// 	bubble_status = 1;
+// 	bubbleDOM.innerHTML = "<img src=\"https://pbs.twimg.com/profile_images/681152691461042177/_PrgDgFA_400x400.jpg\" width=\"200px\" height=\"200px\" >";
+// 	bubbleDOM.style.visibility = 'visible';
+// }
 
-var politicians = [{name:"rand paul", img_url:"https://raw.githubusercontent.com/roycboyc/OpenPolitics/master/AlFranken.png"}];
+var politicians = [
+	{name: "catherine cortez masto", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/CatherineMasto.png?raw=true"},
+	{name: "chuck schumer", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/CharlesSchumer.png?raw=true"},
+	{name: "Darrell Issa", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/DarrellIssa.png?raw=true"},
+	{name: "Elizabeth Warren", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/ElizabethWarren.png?raw=true"},
+	{name: "Kristen Gillibrand", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/KristenGillibrand.png?raw=true"},
+	{name: "Paul Ryan", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/PaulRyan.png?raw=true"},
+	{name: "Ted Cruz", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/TedCruz.png?raw=true"},
+	{name: "Rand Paul", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/RandPaul.png?raw=true"},
+	{name: "Trey Gowdy", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/TreyGowdy.png?raw=true"},
+	{name: "Bernie Sanders", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/BernieSanders.PNG?raw=true"},
+	{name: "Brent Farenthold", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/BlakeFarenhold.PNG?raw=true"},
+	{name: "Charles Dent", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/CharlesDent.PNG?raw=true"},
+	{name: "Claire McCaskill", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/ClarieMcCaskill.PNG?raw=true"},
+	{name: "Cory Booker", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/CoryBooker.PNG?raw=true"},
+	{name: "Devin Nunes", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/DevinNunes.PNG?raw=true"},
+	{name: "Jefferson Sessions", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/JeffersonSessions.PNG?raw=true"},
+	{name: "Joe Donnelly", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/JoeDonnelly.PNG?raw=true"},
+	{name: "John McCain", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/JohnMcChain.PNG?raw=true"},
+	{name: "Chuck Schumer", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/JonathanTester.PNG?raw=true"},
+	{name: "Mike Pompeo", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/MikePompeo.PNG?raw=true"},
+	{name: "Ralph Norman", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/RalphNorman.PNG?raw=true"},
+	{name: "Amy Klobuchar", img_url:"https://raw.githubusercontent.com/roycboyc/OpenPolitics/master/AmyKlobuchar.png?raw=true"},
+	{name: "Barbara Boxer", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/BarbaraBoxer.png?raw=true"},
+	{name: "Chuck Grassley", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/ChuckGrassley.png?raw=true"},
+	{name: "IleanaRos Lehtinen", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/IleanaRos-Lehtinen.png?raw=true"},
+	{name: "Jason Chaffetz", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/JasonChaffetz.png?raw=true"},
+	{name: "John Lewis", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/JohnLewis.png?raw=true"},
+	{name: "Keith Ellison", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/KeithEllison.png?raw=true"},
+	{name: "Lindsey Graham", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/LindseyGraham.png?raw=true"},
+	{name: "Nancy Pelosi", img_url:"https://github.com/roycboyc/OpenPolitics/blob/master/NancyPelosi.png?raw=true"},
+
+
+	];
 
