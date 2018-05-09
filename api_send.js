9a38b24a0d1bbc985c9369a1bc62e9f08d90a8ce
pytseng <tw.pytseng@gmail.com>
2018-05-09 14:21:31 -0400

    initialize

--- api_send.js
+++ api_send.js
@@ -13,28 +13,28 @@ let votes_with_party_pct, end_date_of_position, leader_ship_role, most_recent_tw
 // });
 
 api_button.onclick = function(element) {
-	// $.ajax({ 
-	//    type : "GET", 
-	//    url : "https://api.propublica.org/congress/v1/115/senate/members.json", 
-	//    beforeSend: function(xhr){xhr.setRequestHeader('X-API-Key', 'IzJGY0q3AS4wImC3H3t4vGNLIdMuXC7Jn2CwArlc');},
-	//    success : function(result) { 
-	//        votes_with_party_pct = result["results"][0]["members"][0]["votes_with_party_pct"];
-	//        end_date_of_position = "2019-01-03" //result["results"][0]["members"][0][""];
-	//        leader_ship_role = "Minority Leader"; // result["results"][0]["members"][0][""];
-	//        most_recent_twit = "RT @RepRichardNeal: You can’t upend the tax system without creating serious problems – &amp; we’re already seeing the #GOPTaxScam creating many…";
-	//        twit_account = "NancyPelosi";
-	//        console.log("happy new year!");
-	//        $('#display_div')[0]["innerHTML"] = 
-	//        votes_with_party_pct + "\n" +
-	//        end_date_of_position + "\n" +
-	//        leader_ship_role + "\n" +
-	//        most_recent_twit + "\n" +
-	//        twit_account;
-	//    }, 
-	//    error : function(result) { 
-	//       console.log("errrrrrrrrr");
-	//    } 
-	//  }); 
+	$.ajax({ 
+	   type : "GET", 
+	   url : "https://api.propublica.org/congress/v1/115/senate/members.json", 
+	   beforeSend: function(xhr){xhr.setRequestHeader('X-API-Key', 'IzJGY0q3AS4wImC3H3t4vGNLIdMuXC7Jn2CwArlc');},
+	   success : function(result) { 
+	       votes_with_party_pct = result["results"][0]["members"][0]["votes_with_party_pct"];
+	       end_date_of_position = "2019-01-03" //result["results"][0]["members"][0][""];
+	       leader_ship_role = "Minority Leader"; // result["results"][0]["members"][0][""];
+	       most_recent_twit = "RT @RepRichardNeal: You can’t upend the tax system without creating serious problems – &amp; we’re already seeing the #GOPTaxScam creating many…";
+	       twit_account = "NancyPelosi";
+	       console.log("happy new year!");
+	       $('#display_div')[0]["innerHTML"] = 
+	       votes_with_party_pct + "\n" +
+	       end_date_of_position + "\n" +
+	       leader_ship_role + "\n" +
+	       most_recent_twit + "\n" +
+	       twit_account;
+	   }, 
+	   error : function(result) { 
+	      console.log("errrrrrrrrr");
+	   } 
+	 }); 
 
 
    votes_with_party_pct = "92.3%"; 
@@ -49,6 +49,17 @@ api_button.onclick = function(element) {
    $('#leader_ship_role')[0]["innerHTML"] = leader_ship_role;
    $('#twit_account')[0]["innerHTML"] = twit_account;
 
-	console.log(window.getSelection().toString());
+	
 };
 
+
+	$.ajax({ 
+	   type : "GET", 
+	   url : "https://api.propublica.org/congress/v1/115/senate/members.json", 
+	   beforeSend: function(xhr){xhr.setRequestHeader('X-API-Key', 'IzJGY0q3AS4wImC3H3t4vGNLIdMuXC7Jn2CwArlc');},
+	   success : function(result) { 
+	       console.log(result);
+	   }, 
+	   error : function(result) { 
+	   } 
+	 }); 
--- popup.js
+++ popup.js
@@ -9,34 +9,34 @@ function getSelected() {
 	return false;
 }
 
-// (function(){
-// $("<div id='aBox'></div>").appendTo($("body"));	
-// })();
+(function(){
+$("<div id='aBox'></div>").appendTo($("body"));	
+})();
 
-// window.addEventListener("mouseup", function(event) { 
-// 	var selection = getSelected().toString().toLowerCase();	
-// 	var div = $("#aBox").detach();
-// 	$(div).attr({
-// 		"width":"160px",
-// 		"backgroundColor":"#555",
-// 		"textContent":"Injected"
-// 	});
+window.addEventListener("mouseup", function(event) { 
+	var selection = getSelected().toString().toLowerCase();	
+	var div = $("#aBox").detach();
+	$(div).attr({
+		"width":"160px",
+		"backgroundColor":"#555",
+		"textContent":"Injected"
+	});
 	
-// 	// div.textContent='Injected!';
-// 	// div.style.position = "absolute";
-// 	// div.style.width="160px";
-// 	// div.style.backgroundColor="#555";
-// 	// div.style.color="#fff";
-// 	// div.style.zIndex="999";
-// 	// div.style.display="in-line";
-// 	// div.style.position='relative';
-// 	// div.textAlign="center";
-// 	$("#aBox").detach().appendTo(window.getSelection().focusNode);
-// 	// console.log(selection);
-// 	if (selection == "rand paul"){
-// 		window.getSelection().focusNode.parentElement.appendChild(div);
-// 	}
-// });
+	// div.textContent='Injected!';
+	// div.style.position = "absolute";
+	// div.style.width="160px";
+	// div.style.backgroundColor="#555";
+	// div.style.color="#fff";
+	// div.style.zIndex="999";
+	// div.style.display="in-line";
+	// div.style.position='relative';
+	// div.textAlign="center";
+	$("#aBox").detach().appendTo(window.getSelection().focusNode);
+	// console.log(selection);
+	if (selection == "rand paul"){
+		window.getSelection().focusNode.parentElement.appendChild(div);
+	}
+});
 
 // chrome.tabs.executeScript( {
 //     code: "window.getSelection().toString();"
